53cf5ba0c000133d3c22f4ebc78d4c65fffd4d98
anguswg-ucsb <anguswatters@gmail.com>
2022-11-01 06:05:47 -0700

    structure divrecyear function

--- py/get_structure_divrec.py
+++ py/get_structure_divrec.py
@@ -9,7 +9,18 @@ def get_structure_divrecday(
     end_date      = None,
     api_key       = None
     ):
+    """Request Structure Daily Diversion/Release Records
 
+    Args:
+        wdid (_type_, optional): string, tuple or list of WDIDs code of structure. Defaults to None.
+        wc_identifier (str, optional): string, indicating whether "diversion" or "release" should be returned. Defaults to "diversion".
+        start_date (_type_, optional): string date to request data start point YYYY-MM-DD. Defaults to None, which will return data starting at "1900-01-01".
+        end_date (_type_, optional): string date to request data end point YYYY-MM-DD.. Defaults to None, which will return data ending at the current date.
+        api_key (_type_, optional):  string, optional. If more than maximum number of requests per day is desired, an API key can be obtained from CDSS.. Defaults to None.
+
+    Returns:
+        pandas dataframe object: dataframe of daily structure diversion/releases records 
+    """
     # if no abbreviation is given, return error
     if wdid is None:
         return print("Invalid 'wdid' parameter")
@@ -121,7 +132,18 @@ def get_structure_divrecmonth(
     end_date      = None,
     api_key       = None
     ):
-    
+    """Request Structure Monthly Diversion/Release Records
+
+    Args:
+        wdid (_type_, optional): string, tuple or list of WDIDs code of structure. Defaults to None.
+        wc_identifier (str, optional): string, indicating whether "diversion" or "release" should be returned. Defaults to "diversion".
+        start_date (_type_, optional): string date to request data start point YYYY-MM-DD. Defaults to None, which will return data starting at "1900-01-01".
+        end_date (_type_, optional): string date to request data end point YYYY-MM-DD.. Defaults to None, which will return data ending at the current date.
+        api_key (_type_, optional):  string, optional. If more than maximum number of requests per day is desired, an API key can be obtained from CDSS.. Defaults to None.
+
+    Returns:
+        pandas dataframe object: dataframe of monthly structure diversion/releases records 
+    """
     # if no abbreviation is given, return error
     if wdid is None:
         return print("Invalid 'wdid' parameter")
@@ -220,4 +242,123 @@ def get_structure_divrecmonth(
         else:
             page_index += 1
 
+    return data_df
+
+def get_structure_divrecyear(
+    wdid          = None,
+    wc_identifier = "diversion",
+    start_date    = None,
+    end_date      = None,
+    api_key       = None
+    ):
+    """Request Structure Annual Diversion/Release Records
+
+    Args:
+        wdid (_type_, optional): string, tuple or list of WDIDs code of structure. Defaults to None.
+        wc_identifier (str, optional): string, indicating whether "diversion" or "release" should be returned. Defaults to "diversion".
+        start_date (_type_, optional): string date to request data start point YYYY-MM-DD. Defaults to None, which will return data starting at "1900-01-01".
+        end_date (_type_, optional): string date to request data end point YYYY-MM-DD.. Defaults to None, which will return data ending at the current date.
+        api_key (_type_, optional):  string, optional. If more than maximum number of requests per day is desired, an API key can be obtained from CDSS.. Defaults to None.
+
+    Returns:
+        pandas dataframe object: dataframe of annual structure diversion/releases records 
+    """
+    
+    # if no abbreviation is given, return error
+    if wdid is None:
+        return print("Invalid 'wdid' parameter")
+
+    # if a list of WDIDs, collapse list
+    if type(wdid) == list or type(wdid) == tuple:
+        wdid = [str(x) for x in wdid]
+
+        # join list of county names into single string seperated by plus sign
+        wdid = "%2C+".join(wdid)
+
+        # replace white space w/ plus sign
+        wdid = wdid.replace(" ", "%2C+")
+    else:
+        # if wdid is an int or float, convert to string
+        if type(wdid) == int or type(wdid) == float:
+            wdid = str(wdid)
+
+        if type(wdid) == str:
+            # replace white space w/ %2C+ sign
+            wdid = wdid.replace(" ", "%2C+")
+
+    #  base API URL
+    base = "https://dwr.state.co.us/Rest/GET/api/v2/structures/divrec/divrecyear/?"
+
+    # if no start_date is given, default to 1900-01-01
+    if start_date is None:
+        start_date = "1900-01-01"
+        start_date = datetime.datetime.strptime(start_date, '%Y-%m-%d')
+        start_date = start_date.strftime("%Y")
+        start_date = start_date.replace("-", "%2F")
+    else:
+        start_date = datetime.datetime.strptime(start_date, '%Y-%m-%d')
+        start_date = start_date.strftime("%Y")
+        start_date = start_date.replace("-", "%2F")
+
+    # if no end date is given, default to current date
+    if end_date is None:
+        end_date = datetime.date.today()
+        end_date = end_date.strftime("%Y")
+        end_date = end_date.replace("-", "%2F")
+    else:
+        end_date = datetime.datetime.strptime(end_date, '%Y-%m-%d')
+        end_date = end_date.strftime("%Y")
+        end_date = end_date.replace("-", "%2F")
+
+    # maximum records per page
+    page_size = 50000
+
+    # initialize empty dataframe to store data from multiple pages
+    data_df = pd.DataFrame()
+
+    # initialize first page index
+    page_index = 1
+
+    # Loop through pages until there are no more pages to get
+    more_pages = True
+
+    # Loop through pages until last page of data is found, binding each responce dataframe together
+    while more_pages == True:
+        # create query URL string tuple
+        url = (base, 
+        "format=json&dateFormat=spaceSepToSeconds",
+        "&wcIdentifier=*", wc_identifier,
+        "*&min-dataMeasDate=", start_date,
+        "&max-dataMeasDate=", end_date,
+        "&wdid=", wdid,
+        "&pageSize=", str(page_size),
+        "&pageIndex=", str(page_index)
+        )
+        
+        # concatenate non-None values into query URL
+        url = [x for x in url if x is not None]
+        url = "".join(url)
+
+        # If an API key is provided, add it to query URL
+        if api_key is not None:
+            # Construct query URL w/ API key
+            url = url + "&apiKey=" + str(api_key)
+
+        # make API call
+        cdss_req = requests.get(url)
+
+        # extract dataframe from list column
+        cdss_df = cdss_req.json()
+        cdss_df = pd.DataFrame(cdss_df)
+        cdss_df = cdss_df["ResultList"].apply(pd.Series)
+
+        # bind data from this page
+        data_df = pd.concat([data_df, cdss_df])
+
+        # Check if more pages to get to continue/stop while loop
+        if (len(cdss_df.index) < page_size):
+            more_pages = False
+        else:
+            page_index += 1
+    
     return data_df
