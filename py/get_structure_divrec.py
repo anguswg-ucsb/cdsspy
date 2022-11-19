f26f7782e28c2c0ed414ff04ae5eb6d8a2626c0a
anguswg-ucsb <anguswatters@gmail.com>
2022-11-19 07:53:53 -0800

    get structure stage, volume function

--- py/get_structure_divrec.py
+++ py/get_structure_divrec.py
@@ -360,3 +360,100 @@ def get_structure_divrecyear(
             page_index += 1
     
     return data_df
+
+def get_structure_stage(
+    wdid          = None,
+    start_date    = None,
+    end_date      = None,
+    api_key       = None
+    ):
+    """Request Structure stage/volume Records
+
+    Args:
+        wdid (str, optional): string, tuple or list of WDIDs code of structure. Defaults to None.
+        start_date (str, optional): string date to request data start point YYYY-MM-DD. Defaults to None, which will return data starting at "1900-01-01".
+        end_date (str, optional): string date to request data end point YYYY-MM-DD.. Defaults to None, which will return data ending at the current date.
+        api_key (str, optional):  string, optional. If more than maximum number of requests per day is desired, an API key can be obtained from CDSS.. Defaults to None.
+
+    Returns:
+        pandas dataframe object: dataframe of daily structure stage/volume records 
+    """
+
+    # if no abbreviation is given, return error
+    if wdid is None:
+        return print("Invalid 'wdid' parameter")
+
+    #  base API URL
+    base = "https://dwr.state.co.us/Rest/GET/api/v2/structures/divrec/stagevolume/?"
+
+    # collapse list, tuple, vector of wdid into query formatted string
+    wdid = collapse_vector(
+        vect = wdid, 
+        sep  = "%2C+"
+        )
+
+    # parse start_date into query string format
+    start_date = parse_date(
+        date   = start_date,
+        start  = True,
+        format = "%m-%d-%Y"
+    )
+
+    # parse end_date into query string format
+    end_date = parse_date(
+        date   = end_date,
+        start  = False,
+        format = "%m-%d-%Y"
+        )
+
+    # maximum records per page
+    page_size = 50000
+
+    # initialize empty dataframe to store data from multiple pages
+    data_df = pd.DataFrame()
+
+    # initialize first page index
+    page_index = 1
+
+    # Loop through pages until there are no more pages to get
+    more_pages = True
+        # Loop through pages until last page of data is found, binding each responce dataframe together
+    while more_pages == True:
+
+        # create string tuple
+        url = (base, 
+        "format=json&dateFormat=spaceSepToSeconds",
+        "&min-dataMeasDate=", start_date,
+        "&max-dataMeasDate=", end_date,
+        "&wdid=", wdid,
+        "&pageSize=", str(page_size),
+        "&pageIndex=", str(page_index)
+        )
+
+        # concatenate non-None values into query URL
+        url = [x for x in url if x is not None]
+        url = "".join(url)
+
+        # If an API key is provided, add it to query URL
+        if api_key is not None:
+            # Construct query URL w/ API key
+            url = url + "&apiKey=" + str(api_key)
+
+        # make API call
+        cdss_req = requests.get(url)
+
+        # extract dataframe from list column
+        cdss_df = cdss_req.json()
+        cdss_df = pd.DataFrame(cdss_df)
+        cdss_df = cdss_df["ResultList"].apply(pd.Series)
+
+        # bind data from this page
+        data_df = pd.concat([data_df, cdss_df])
+
+        # Check if more pages to get to continue/stop while loop
+        if (len(cdss_df.index) < page_size):
+            more_pages = False
+        else:
+            page_index += 1
+    
+    return data_df
