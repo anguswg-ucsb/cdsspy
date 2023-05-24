11d26bcfde86804530ba1fff0d88281c02f450c3
anguswg-ucsb <anguswatters@gmail.com>
2023-05-24 08:32:25 -0700

    updating package structure in dev branch

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cdsspy",                     # This is the name of the package
-    version="1.2.64",                        # The initial release version
+    version="1.2.66",                        # The initial release version
     author="Angus Watters",                     # Full name of the author
     description="Provides Python functions for discovering and requesting data from the CDSS REST API.",
     long_description=long_description,      # Long description read from the the readme file
     long_description_content_type="text/markdown",
-    packages=setuptools.find_packages(),    # List of all python modules to be installed
+    packages=setuptools.find_packages(exclude=["cdsspy.egg_info"]),    # List of all python modules to be installed
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
@@ -18,6 +18,6 @@ setuptools.setup(
     ],                                      # Information to filter the project on PyPi website
     python_requires='>=3.6',                # Minimum version requirement of the package
     py_modules=["cdsspy"],             # Name of the python package
-    package_dir={'':'src/cdsspy'},     # Directory of the source code of the package
+    package_dir={'':'src'},     # Directory of the source code of the package
     install_requires=['pandas', 'datetime', 'requests', 'geopandas', 'shapely', 'pyproj']
 )
